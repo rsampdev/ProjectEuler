74ee4b79471a184d113ea023910139b7c0c2cd9d
rsampdev <rsampdev@gmail.com>
2016-09-21 09:53:43 -0400

    Initial Commit

---
+++ ProjectEuler/ProjectEuler.h
@@ -0,0 +1,19 @@
+//
+//  ProjectEuler.h
+//  ProjectEuler
+//
+//  Created by Rodney Sampson on 9/21/16.
+//  Copyright © 2016 Rodney Sampson II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProjectEuler.
+FOUNDATION_EXPORT double ProjectEulerVersionNumber;
+
+//! Project version string for ProjectEuler.
+FOUNDATION_EXPORT const unsigned char ProjectEulerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectEuler/PublicHeader.h>
+
+
